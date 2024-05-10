65b20c2975b3be05bb946ece059fdc3df94d9521
Yue tl <kimthach.work@gmail.com>
2024-05-10 19:46:48 +0700

    add SMPT

---
+++ PBL3/src/main/java/UI/SendEmail.java
@@ -0,0 +1,39 @@
+package UI;
+
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail {
+    public static void main(String[] args) {
+        String myEmail = "yueeeee404@gmail.com";
+        String password = "kbwd dhai ldhc lsyp"; // Mật khẩu email của bạn
+        String toEmail = "yue.moee@gmail.com";
+        String host = "smtp.gmail.com";
+        String port = "587"; 
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myEmail, password);
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject("Test Email from Java");
+            String emailContent = "This is a test email sent from Java.";
+            message.setText(emailContent);
+            Transport.send(message);
+            System.out.println("Email sent successfully!");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
