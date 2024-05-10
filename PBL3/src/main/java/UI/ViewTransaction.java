c61b43f68fec81433ee8b992128f51acf9c18def
quyle <huuquyle9@gmail.com>
2024-05-11 03:23:44 +0700

    quyle 11.05.2024

--- PBL3/src/main/java/UI/ViewTransaction.java
+++ PBL3/src/main/java/UI/ViewTransaction.java
@@ -5,6 +5,10 @@
 package UI;
 
 import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.util.List;
+import persistence.*;
+import utils.Zdata;
 
 /**
  *
@@ -21,8 +25,31 @@ public class ViewTransaction extends javax.swing.JFrame {
     }
     
     public void showPieChart(){
+        
         DefaultTableModel model = (DefaultTableModel)jTable2.getModel();
         model.setRowCount(0);
+        List<Return> returns = Zdata.returnDao.getAll();
+        List<Borrow> borrows = Zdata.borrowDao.getAll();
+        for (Return returnn : returns) {
+            Object[] row = {
+                returnn.getLoanId(),
+                returnn.getUserId(),
+                returnn.getBookId(),
+                returnn.getReturnTime(),
+                "---"
+            };
+            model.addRow(row);
+        }
+        for (Borrow borrow : borrows) {
+            Object[] row = {
+                borrow.getLoanId(),
+                borrow.getUserId(),
+                borrow.getBookId(),
+                borrow.getReleaseTime(),
+                borrow.getDueTime()
+            };
+            model.addRow(row);
+        }
         
     }
 
